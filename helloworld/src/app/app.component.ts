2ed5a186aeec0f8e0616a2782e8dae2289466ac9
Navinashok <navinashok.sg@gmail.com>
2023-08-19 19:12:05 +0530

    passing data to and getting response from server

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';//in quotes
 
 
 @Component({
@@ -12,9 +13,26 @@ export class AppComponent {
   name="";
   selectClause="";//imp neccessary for {{}}
   whereClause="";
+  query=""
+  constructor(private http:HttpClient)
+  {
+
+  }
+  geturl="http://localhost:5000/backend";
+  request="";
+  resp={};
+
+
   onSubmitQuery( some :{ cols :string , rows:string })
   {
-    console.warn("done");
+
+    this.query="/select "+this.selectClause+" from table where "+this.whereClause;
+    this.request=this.geturl+this.query;
+
+    this.http.get(this.request).subscribe(
+      (res) =>{console.log(res);this.resp=res}
+    )
+
   }
 
 }
--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
