0ed541072284fdcd6863972f517a8029375c9903
Navinashok <navinashok.sg@gmail.com>
2023-08-19 16:30:33 +0530

    forms

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'helloworld';
+  name="";
+  selectClause="";//imp neccessary for {{}}
+  whereClause="";
+  onSubmitQuery( some :{ cols :string , rows:string })
+  {
+    console.warn("done");
+  }
+
 }
